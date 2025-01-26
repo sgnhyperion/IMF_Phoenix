7a2c68512e93e816e3f423e73ff4e0ddb8539657
Harsh <hk755063@gmail.com>
2025-01-26 20:31:00 +0530

    minor change

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import { initializeDatabase } from './lib/db.js';
 import authRoute from './route/auth.route.js';
 import gadgetRoute from './route/gadget.route.js';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import setupSwagger from './config/swagger.config.js';
 
@@ -13,10 +12,7 @@ const app = express();
 
 setupSwagger(app);
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+
 app.use(express.json());
 app.use(cookieParser());
 
