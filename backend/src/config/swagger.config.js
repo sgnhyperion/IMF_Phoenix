4b788e2ba5a8146a20497f6bdd9bb0fe79fa6ff2
Harsh <hk755063@gmail.com>
2025-02-13 07:35:40 +0530

    config update

--- backend/src/config/swagger.config.js
+++ backend/src/config/swagger.config.js
@@ -16,8 +16,8 @@ const options = {
     servers: [
       {
         url: process.env.NODE_ENV === 'production' 
-          ? 'your-production-url' 
-          : 'https://imf-phoenix.onrender.com', // replace it with http://loaclhost:5000 to run it locally
+          ? 'https://imf-phoenix.onrender.com' 
+          : 'http://localhost:5000', 
         description: process.env.NODE_ENV === 'production' ? 'Production server' : 'Development server'
       }
     ],
